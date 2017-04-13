58a24021b1d7cef6fa9ad8281ff908e2b4d89352
Nicolas Calle <ncalle@ucsd.edu>
2017-04-12 21:18:52 -0700

    setup basic react code

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,49 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class RouteImg extends Component{
+    render() {
+        return <img src={this.props.src} />
+    }
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props){
+        super(props);
+        this.state = {
+            images: []
+        }
+        this.getImages();
+    }
+    render() {
+        var imgList = Array();
+        for (var i in this.state.images){
+            imgList.push(<RouteImg key={i} src={this.state.images[i]}/>)
+        }
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <ul>
+                    {imgList}
+                </ul>
+
+            </div>
+        );
+    }
+    getImages(){
+        var s = this;
+        fetch("/bikeRoute").then(function(response){
+            return response.json();
+        }).then(function(j){
+            s.setState({images: j})
+        })
+    }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -2,7 +2,17 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+var request = require('request');
+var GoogleMapsAPI = require('googlemaps');
 var port = process.env.port || 3001;
+
+
+
+var APIKEY = "AIzaSyCuJpY-A-LadO95r90DWf0_HMALJTO2KYg";
+var publicConfig = {key: APIKEY}
+var gmAPI = new GoogleMapsAPI(publicConfig);
+
+var baseurl = "https://maps.googleapis.com/maps/api/streetview?"
 app.use(express.static(path.join(__dirname, '/static')));
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -12,6 +22,38 @@ app.use(bodyParser.urlencoded({extended: true}));
 // app.get('/', function(req, res){
 //     res.render('index');
 // });
+
+app.get('/bikeRoute', function(req,res){
+    //Test locations along torrey pines rd
+    var locations = [
+        "32.9302117,-117.2598747",
+        "32.9305074,-117.2599049",
+        "32.9306982,-117.2599242",
+        "32.9307913,-117.2599337",
+        "32.9309801,-117.2599538",
+        "32.9310748,-117.2599642",
+        "32.9312666,-117.2599888",
+        "32.9314699,-117.2600164",
+        "32.9318718,-117.2600714",
+        "32.9323088,-117.2601319",
+        "32.9326816,-117.2601851",
+        "32.9328687,-117.2602141",
+        "32.9330552,-117.2602482",
+        "32.9336309,-117.2602437"
+    ]
+    var images = Array(locations.length);
+    for(var i = 0; i < locations.length; i++){
+        params = {
+            location: locations[i],
+            size: "600x300",
+            heading: "0",
+            fov: "120",
+        }
+        images[i] = gmAPI.streetView(params)
+    }
+    res.send(images);
+
+})
 
 app.listen(port, function(){
     console.log("listening in on port " + port);
