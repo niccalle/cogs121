f0effc8a5af4d8112357b6a0d1c905b80ab3df42
Nicolas Calle <ncalle@ucsd.edu>
2017-04-17 17:10:55 -0700

    faster image loading

--- client/src/RouteSearch.js
+++ client/src/RouteSearch.js
@@ -11,7 +11,7 @@ import { Col } from 'react-bootstrap';
 
 class RouteImg extends Component{
     render() {
-        return <img src={this.props.src} onClick={this.props.handleClick} alt="test"/>
+        return <img src={this.props.src} style={this.props.style} onClick={this.props.handleClick} alt="test"/>
     }
 }
 
@@ -20,20 +20,24 @@ class RouteSearch extends Component{
         super(props);
         this.state = {
             images: [],
-            imgs: [],
             start: '',
             end: '',
-            index: 0
+            index: 0,
+            playing: false
         }
         //Binding so we can call 'this' inside the methods
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-
+        this.playRoute = this.playRoute.bind(this);
         //Load the intial images
         this.getImages();
     }
 
     render(){
+        var imgs = []
+        for(var i in this.state.images){
+            imgs.push(this.renderImage(i));
+        }
         return (
             <div className="container">
                 <div className="start-end">
@@ -57,9 +61,16 @@ class RouteSearch extends Component{
                 </div>
                 <div className="route-gif">
                     <ul>
-                        {this.state.imgs[this.state.index]}
+                        {imgs}
                     </ul>
                 </div>
+                <Row className="play-route">
+                    <Col md={2}>
+                        <Button bsStyle="danger" onClick={this.playRoute} block>
+                            Play!
+                        </Button>
+                    </Col>
+                </Row>
             </div>
         )
     }
@@ -82,12 +93,7 @@ class RouteSearch extends Component{
             return response.json();
         }).then(function(j){
             var imgs = []
-
-            for (var i in j){
-                imgs.push(<RouteImg key={i} src={j[i]} handleClick={() => s.handleClick()}  />);
-            }
-
-            s.setState({images: j, imgs: imgs})
+            s.setState({images: j})
         })
     }
 
@@ -96,19 +102,32 @@ class RouteSearch extends Component{
         this.setState({index: (this.state.index + 1)%this.state.images.length});
     }
 
+    renderImage(i) {
+        var style = {
+            "display": i == this.state.index ? "inline" : "none"
+        }
+        return <RouteImg key={i} src={this.state.images[i]} style={style} handleClick={() => this.handleClick()}/>;
+    }
+
     getImages(){
         var s = this;
         fetch("/bikeRoute").then(function(response){
             return response.json();
         }).then(function(j){
             var imgs = []
-
-            for (var i in j){
-                imgs.push(<RouteImg key={i} src={j[i]} handleClick={() => s.handleClick()}  />);
-            }
-
-            s.setState({images: j, imgs: imgs})
+            s.setState({images: j})
         })
+    }
+
+    playRoute(){
+        if(!this.state.playing){
+            this.interval = setInterval(() => this.handleClick(), 100);
+            this.setState({playing: true});
+        }
+        else{
+            clearInterval(this.interval);
+            this.setState({playing: false});
+        }
     }
 }
 
